a95397cf5a53a6c5b19b4e610fa81051438379df
LuukS <Luukschouten01@gmail.com>
2022-05-12 15:18:38 +0200

    started questions page and progress bar

---
+++ Stemwijzer.js
@@ -0,0 +1,13 @@
+// define back button 
+var backBtn = document.getElementById("backBtn");
+//define continue button
+var continueBtn = document.getElementsByClassName("continueBtn");
+
+
+backBtn.addEventListener("click", function(){
+    
+});
+
+continueBtn.addEventListener("click", function(){s
+        
+    });
