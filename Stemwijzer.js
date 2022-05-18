ab3f5695e4b815a38c13f98218bb8089a1a232a2
LuukS <Luukschouten01@gmail.com>
2022-05-18 20:38:38 +0200

    added a bit of js

--- Stemwijzer.js
+++ Stemwijzer.js
@@ -1,1 +1,25 @@
-console.log(parties[0].name);
+var stellingNmr = 0;
+
+var stellingHeader = document.getElementById("stellingHead");
+var stelling = document.getElementById("stelling");
+
+updateStelling();
+
+function updateStelling(){
+    for(i=0; i < subjects.length; i++){
+        if(stellingNmr == i){
+            stellingHeader.innerHTML = i+1 + ". " + subjects[stellingNmr].title;
+            stelling.innerHTML = subjects[stellingNmr].statement;
+        }
+    }
+}
+
+function next(){
+    console.log('amogus');
+    stellingNmr += 1;
+    updateStelling();
+}
+
+function test(){
+    console.log('test');
+}
